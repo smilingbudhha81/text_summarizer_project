94b82a5aab9eeccea0a4328479047ec3fe827ca6
smilingbudhha81 <rvaishnav81.rv@gmail.com>
2023-10-04 21:31:31 +0530

    requirements & installation done

--- setup.py
+++ setup.py
@@ -6,25 +6,26 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME =  "End-to-end-Text-Summarization"
+REPO_NAME = "text_summarizer_project"
 AUTHOR_USER_NAME = "smilingbudhha81"
-SRC_REPO = "text_summarizer_project"
+SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "rvaishnav81.rv@gmail.com"
 
+
+
 setuptools.setup(
-    name = SRC_REPO,
-    version = __version__,
+    name=SRC_REPO,
+    version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package for NLP app",
     long_description=long_description,
     long_description_content="text/markdown",
-    url= f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
 
     },
-    package_dir={"":"src"},
-    packages = setuptools.find_packages(where="src")
-
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
